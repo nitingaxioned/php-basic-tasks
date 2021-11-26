5b2b3a5a058d3d354ed9f14c720c68422f18f645
Nitin <niting@axioned.com>
2021-11-26 19:05:42 +0530

    task 8 created

--- Array/task08.php
+++ Array/task08.php
@@ -1,5 +1,12 @@
 <?php
-
     #Write a PHP script to get an array containing all the entries of an array which have the keys that are present in another array.
     
+    $arr = array('c1' => 'Red', 'c2' => 'Green', 'c3' => 'White', 'c4' => 'Black'); 
+    $keys = array('c2', 'c4'); 
+    $nwe_arr = [];
+    foreach($keys as $val){
+        $nwe_arr[$val] = $arr[$val];
+    }
+
+    print_r($nwe_arr);
 ?>
